9e0ae4f71b1df52bbd519b76591c2233d41951fe
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2025-01-17 11:44:51 +0530

    Add files via upload

---
+++ DayFour.js
@@ -0,0 +1,62 @@
+/*const add=(a,b) =>a+b
+//console.log(add(5,10))
+res=add(5,10)
+console.log(res)
+*/
+
+
+// const msg=()=>{
+//     console.log("Greetings")
+// }
+
+
+// const test=(x,y,z=4)=>{
+//     console.log(x+y+z)
+// }
+// test(5,4)
+
+
+// const createObj = ({ key, value }) => {
+//     return {
+//         [key]: value
+//     };
+// };
+
+// const person = createObj({ key: 'name', value: 'value' });
+// console.log(person);
+
+
+// let propName='c';
+// const rank={
+//     a:1,
+//     b:2,
+//     [propName]:3
+// }
+// console.log(rank);
+
+
+// let LAST_NAME = "lastname";
+// let fullname = {
+//     firstname: "Rahul",
+//     [LAST_NAME]: "Dev"
+// };
+// console.log("My fullname is: " + fullname.firstname + " " + fullname[LAST_NAME]);
+
+
+
+const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium']; 
+console.log(materials.map((material) => material.length));
+
+// const square = (num) => num * num;
+// console.log(square(5)); 
+
+
+// const numbers = [1, 2, 3];
+// const squares = numbers.map(num => num * num);
+// console.log(squares);
+
+
+// const reverseString = (str) => str.split('').reverse().join('');
+// console.log(reverseString("hello"));
+
+
